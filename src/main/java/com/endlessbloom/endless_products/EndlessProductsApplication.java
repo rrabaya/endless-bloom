f17d7b1cdb551a2a5825271ca8aeff6de1c6863f
Reivin <RMRabaya@securitybank.com.ph>
2025-02-03 17:15:15 +0800

    initial

---
+++ src/main/java/com/endlessbloom/endless_products/EndlessProductsApplication.java
@@ -0,0 +1,13 @@
+package com.endlessbloom.endless_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndlessProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndlessProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/endlessbloom/endless_products/EndlessProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endlessbloom.endless_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndlessProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
